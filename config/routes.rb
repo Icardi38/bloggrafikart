1d3593f1d2e24a0ed98933e6b4d1cf3ac82a72a5
Icardi38 <vm26190@hotmail.fr>
2018-02-05 11:00:20 +0100

    créer page article

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   root 'pages#index'
-  
+  get '/articles', to: 'posts#index', as: 'posts'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
