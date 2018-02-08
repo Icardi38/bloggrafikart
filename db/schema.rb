ac7d9d4430f768b0df1ff738f31971938b042f58
Icardi38 <vm26190@hotmail.fr>
2018-02-08 09:28:51 +0100

    otre chgmnt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205131349) do
+ActiveRecord::Schema.define(version: 20180206133519) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
